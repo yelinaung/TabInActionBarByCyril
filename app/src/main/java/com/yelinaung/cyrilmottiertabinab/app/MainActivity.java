39765fb927c3cbb236264ec4c6555386e849b3fe
Ye Lin Aung <me@yelinaung.com>
2014-09-06 23:52:06 +0630

    update app build.gradle

--- app/src/main/java/com/yelinaung/cyrilmottiertabinab/app/MainActivity.java
+++ app/src/main/java/com/yelinaung/cyrilmottiertabinab/app/MainActivity.java
@@ -1,12 +1,12 @@
 package com.yelinaung.cyrilmottiertabinab.app;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
